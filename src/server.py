4f58306b33f34b28e8812577d1c63efb7602c73f
linrol <linrol@77hub.com>
2021-06-08 20:07:25 +0800

    修改监听的端口号

--- src/server.py
+++ src/server.py
@@ -51,7 +51,7 @@ async def view(request: web.Request, bot: Wechaty):
 
     return web.json_response(data=dict(msg='Success'))
 
-async def puppetware_start_server(bot, host='0.0.0.0', port=8082):
+async def puppetware_start_server(bot, host='0.0.0.0', port=8080):
     context['bot'] = bot
 
     app = web.Application()
